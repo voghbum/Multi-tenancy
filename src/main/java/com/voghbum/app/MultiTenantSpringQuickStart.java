3acf1b2f7de6fe5a1960f9c0d066a4eb7f52ec3f
huseyin.asci <huseyinascci@gmail.com>
2025-04-23 17:37:29 +0300

    Bauldung daki yazının jwt kısmına kadar olanı gerçeklendi
    https://www.baeldung.com/multitenancy-with-spring-data-jpa
    
    Fakat çalışmıyor. Spring application.properties'de datasource ayarları arıyor gibi. Github'daki proje baz alınarak değiştirilecek.

---
+++ src/main/java/com/voghbum/app/MultiTenantSpringQuickStart.java
@@ -0,0 +1,11 @@
+package com.voghbum.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantSpringQuickStart {
+    public static void main(String[] args) {
+        SpringApplication.run(MultiTenantSpringQuickStart.class, args);
+    }
+}
